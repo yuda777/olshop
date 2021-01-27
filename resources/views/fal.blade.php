5c3d8d6c9d337adf3b15ec4210c8fc4cbc305adb
Hendri Yuda <zerone777@gmail.com>
2021-01-27 15:07:08 +0700

    flickity2

---
+++ resources/views/fal.blade.php
@@ -0,0 +1,25 @@
+<h1>Flickity - side vertical navigation</h1>
+
+<div class="carousel-container">
+  <!-- Flickity HTML init -->
+  <div class="carousel carousel-main">
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+  </div>
+
+  <div class="carousel-nav">
+    <div class="carousel-cell is-nav-selected"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+    <div class="carousel-cell"></div>
+  </div>
+
+</div>
